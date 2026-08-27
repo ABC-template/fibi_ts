250a6b7ab3a035303cbe209a360ea6cd291905b8
ABC-template <62172120+ABC-template@users.noreply.github.com>
2026-08-27 22:18:38 +0400

    Create index.ts

---
+++ api/admin/economy/index.ts
@@ -0,0 +1,8 @@
+// ============================================
+// api/admin/economy/index.ts
+// Экспорт админских эндпоинтов экономики
+// Версия: 1.0.0
+// ============================================
+
+export { default as config } from './config';
+export { default as whitelist } from './whitelist';
