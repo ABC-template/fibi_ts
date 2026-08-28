8cc93b420ea50c962e7e00ae5567389982afdfb0
ABC-template <62172120+ABC-template@users.noreply.github.com>
2026-08-28 11:08:22 +0400

    Update index.ts

--- api/admin/economy/index.ts
+++ api/admin/economy/index.ts
@@ -6,3 +6,7 @@
 
 export { default as config } from './config';
 export { default as whitelist } from './whitelist';
+export { default as limits } from './limits';
+export { default as settings } from './settings';
+export { default as subscriptions } from './subscriptions';
+export { default as blocks } from './blocks';
